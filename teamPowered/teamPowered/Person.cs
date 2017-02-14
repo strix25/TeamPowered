da82a413de2216814e63f89d74af75f55b8fd9dc
Damjan <damjan.oslaj@gmail.com>
2017-02-14 19:30:09 +0100

    split name

--- teamPowered/teamPowered/Person.cs
+++ teamPowered/teamPowered/Person.cs
@@ -8,7 +8,7 @@ namespace teamPowered
 {
     class Person
     {
-        public string Name { get; set; }
+        public string FirstName { get; set; }
         public string Address { get; set; }
     }
 }
